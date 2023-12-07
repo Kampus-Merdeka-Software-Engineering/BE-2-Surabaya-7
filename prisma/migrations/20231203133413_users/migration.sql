713ff470b78c8ad50d2c5be0e4f4bb6c176c5e94
Ziad Zenzayyid <139895583+Imurfrens@users.noreply.github.com>
2023-12-08 02:59:36 +0700

    update lagi 4

--- prisma/migrations/20231203133413_users/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE `users` (
-    `user_id` INTEGER NOT NULL AUTO_INCREMENT,
-    `username` VARCHAR(100) NOT NULL,
-    `email` VARCHAR(100) NOT NULL,
-
-    PRIMARY KEY (`user_id`)
-) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
