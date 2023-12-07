0c937c68d58c53d02cad9bf140a8a8fc07c44a3c
Ziad Zenzayyid <139895583+Imurfrens@users.noreply.github.com>
2023-12-08 02:36:08 +0700

    update lagi

--- config/database.js
+++ config/database.js
@@ -1,11 +1,27 @@
 import { Sequelize } from "sequelize";
 import "dotenv/config.js"
 
-const db = new Sequelize('users', 'root', 'qwerty0403', {
-  host: 'localhost',
-  dialect: 'mysql',
-  port: 3306,
-});
-
+const db = new Sequelize(
+  process.env.DATABASE_NAME,
+  process.env.DATABASE_USERNAME,
+  process.env.DATABASE_PASSWORD,
+  {
+    host: process.env.DATABASE_HOST,
+    dialect: "mysql",
+    port: process.env.DATABASE_PORT,
+  }
+);
 
 export default db;
+
+// import { Sequelize } from "sequelize";
+// import "dotenv/config.js"
+
+// const db = new Sequelize('users', 'root', 'qwerty0403', {
+//   host: 'localhost',
+//   dialect: 'mysql',
+//   port: 3306,
+// });
+
+
+// export default db;
